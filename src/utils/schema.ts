a2fc33b819a2fb76cc0c9608f301bfea08a33491
sagarchauhan1677-dev <sagarchauhan1677@gmail.com>
2026-08-20 23:12:15 +0530

    Fix business identity and local service schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,55 +1,57 @@
 export const organizationSchema = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  "name": "J.D Enterprise",
-  "url": "https://jdfabrication.co.in",
-  "logo": "https://jdfabrication.co.in/jd-enterprise-logo.svg",
+  "@id": "https://jdfabrication.co.in/#organization",
+  "name": "J.D. Enterprise",
+  "alternateName": "J.D. Enterprise Fabrication Works",
+  "url": "https://jdfabrication.co.in/",
+  "logo": { "@type": "ImageObject", "url": "https://jdfabrication.co.in/jd-enterprise-logo.svg" },
   "foundingDate": "1996",
   "slogan": "Just Think We Do",
-  "knowsAbout": ["Industrial fabrication", "Steel fabrication", "Industrial sheds", "PEB buildings", "Warehouse construction", "Roofing and cladding"],
-  "description": "J.D Enterprise is a leading industrial fabrication and construction company in Ahmedabad, Gujarat, India specializing in industrial sheds, PEB buildings, steel fabrication, warehouse construction and roofing systems.",
+  "knowsAbout": ["Industrial fabrication", "MS steel fabrication", "Structural steel fabrication", "Industrial sheds", "PEB buildings", "Warehouse construction", "Roofing and cladding"],
+  "description": "J.D. Enterprise is an industrial fabrication and construction company in Ahmedabad, Gujarat specialising in industrial sheds, PEB buildings, MS and structural steel fabrication, warehouse construction, roofing and cladding.",
   "address": {
     "@type": "PostalAddress",
-    "streetAddress": "17/A J.D Enterprise, Shahpur Society, Opp Master Colony",
+    "streetAddress": "17/A J.D. Enterprise, Shahpur Society, Opp Master Colony",
     "addressLocality": "Ahmedabad",
     "addressRegion": "Gujarat",
     "postalCode": "380004",
     "addressCountry": "IN"
   },
   "telephone": "+91-9099099916",
-  "email": "sagar.chauhan@jdfabrication.co.in",
-  "sameAs": [
-    "https://www.facebook.com/Jashdhan1998",
-    "https://www.instagram.com/j.d.enterprise1677/",
-    "https://x.com/JDENTERPRISE007"
-  ],
+  "email": "jd1677@jdfabrication.co.in",
+  "sameAs": ["https://www.facebook.com/Jashdhan1998", "https://www.instagram.com/j.d.enterprise1677/", "https://x.com/JDENTERPRISE007"],
   "areaServed": [
     { "@type": "City", "name": "Ahmedabad" },
-    { "@type": "State", "name": "Gujarat" },
-    { "@type": "Country", "name": "India" }
+    { "@type": "City", "name": "Sanand" },
+    { "@type": "Place", "name": "Changodar" },
+    { "@type": "Place", "name": "Chhatral" },
+    { "@type": "City", "name": "Vadodara" },
+    { "@type": "State", "name": "Gujarat" }
   ]
 };
 
 export const localBusinessSchema = {
   "@context": "https://schema.org",
   "@type": "LocalBusiness",
-  "name": "J.D Enterprise",
+  "@id": "https://jdfabrication.co.in/#localbusiness",
+  "name": "J.D. Enterprise",
+  "alternateName": "J.D. Enterprise Fabrication Works",
   "image": "https://jdfabrication.co.in/jd-enterprise-logo.svg",
-  "url": "https://jdfabrication.co.in",
+  "url": "https://jdfabrication.co.in/",
   "telephone": "+91-9099099916",
-  "email": "sagar.chauhan@jdfabrication.co.in",
+  "email": "jd1677@jdfabrication.co.in",
   "priceRange": "₹₹",
-  "openingHoursSpecification": [
-    {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-      "opens": "09:00",
-      "closes": "18:00"
-    }
-  ],
+  "parentOrganization": { "@id": "https://jdfabrication.co.in/#organization" },
+  "openingHoursSpecification": [{
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    "opens": "09:00",
+    "closes": "18:00"
+  }],
   "address": {
     "@type": "PostalAddress",
-    "streetAddress": "17/A J.D Enterprise, Shahpur Society, Opp Master Colony",
+    "streetAddress": "17/A J.D. Enterprise, Shahpur Society, Opp Master Colony",
     "addressLocality": "Ahmedabad",
     "addressRegion": "Gujarat",
     "postalCode": "380004",
@@ -57,25 +59,37 @@ export const localBusinessSchema = {
   },
   "areaServed": [
     { "@type": "City", "name": "Ahmedabad" },
-    { "@type": "State", "name": "Gujarat" },
-    { "@type": "Country", "name": "India" }
+    { "@type": "City", "name": "Sanand" },
+    { "@type": "Place", "name": "Changodar" },
+    { "@type": "Place", "name": "Chhatral" },
+    { "@type": "City", "name": "Vadodara" },
+    { "@type": "State", "name": "Gujarat" }
   ],
-  "description": "Industrial fabrication and construction company in Ahmedabad, Gujarat offering industrial shed fabrication, PEB buildings, steel fabrication, warehouse construction and roofing & cladding services."
+  "hasOfferCatalog": {
+    "@type": "OfferCatalog",
+    "name": "Industrial Fabrication Services",
+    "itemListElement": [
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Industrial Shed Fabrication", "url": "https://jdfabrication.co.in/services/industrial-shed-fabrication" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "PEB Buildings", "url": "https://jdfabrication.co.in/services/peb-buildings" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "MS and Structural Steel Fabrication", "url": "https://jdfabrication.co.in/services/steel-fabrication" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Warehouse Construction", "url": "https://jdfabrication.co.in/services/warehouse-construction" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Roofing and Cladding", "url": "https://jdfabrication.co.in/services/roofing-cladding" } },
+      { "@type": "Offer", "itemOffered": { "@type": "Service", "name": "Structural Works", "url": "https://jdfabrication.co.in/services/structural-works" } }
+    ]
+  },
+  "description": "Industrial fabricators in Ahmedabad providing industrial shed fabrication, PEB buildings, MS and structural steel fabrication, warehouse construction, roofing and cladding across Gujarat."
 };
 
 export const serviceSchema = {
   "@context": "https://schema.org",
   "@type": "Service",
+  "@id": "https://jdfabrication.co.in/services#service",
   "name": "Industrial Fabrication and Construction Services",
   "serviceType": "Industrial fabrication and construction",
-  "provider": {
-    "@type": "LocalBusiness",
-    "name": "J.D Enterprise"
-  },
+  "provider": { "@id": "https://jdfabrication.co.in/#localbusiness" },
   "areaServed": [
     { "@type": "City", "name": "Ahmedabad" },
-    { "@type": "State", "name": "Gujarat" },
-    { "@type": "Country", "name": "India" }
+    { "@type": "State", "name": "Gujarat" }
   ],
   "url": "https://jdfabrication.co.in/services"
 };
